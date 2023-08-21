b06006f3703e8375b7ec41ca0c2d42895cd7bf94
GIT13Olena <girnyak.1olena@gmail.com>
2023-08-21 14:45:21 +0300

    05 avatarUser

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,14 +1,28 @@
 const express = require("express");
 const router = express.Router();
 const authenticate = require("../../middleware/authenticate");
+const multer = require("multer");
 const {
   registerUser,
   loginUser,
   getCurrentUser,
+  updateAvatar,
 } = require("../../controllers/users");
+
+const storage = multer.diskStorage({
+  destination: "public/avatars", 
+  filename: (req, file, cb) => {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, uniqueSuffix + path.extname(file.originalname));
+  },
+});
+
+const upload = multer({ storage });
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.get("/current", authenticate, getCurrentUser);
+
+router.patch("/avatars", authenticate, upload.single("avatar"), updateAvatar);
 
 module.exports = router;
